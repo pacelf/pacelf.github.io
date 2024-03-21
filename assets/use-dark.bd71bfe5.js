1d27fef4043158f24141ef6366a3b345eeb56797
Pauline Lawrey <pauline.lawrey@jcu.edu.au>
2024-03-21 13:51:32 +1000

    Version 8 of the PacELF library. Upgraded software used to build the library and changed the look.

---
+++ assets/use-dark.bd71bfe5.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index.97c51684.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
---
+++ assets/use-size.8c1e5cd2.js
@@ -0,0 +1,1 @@
+import{q as s}from"./index.97c51684.js";const t={xs:18,sm:24,md:32,lg:38,xl:46},u={size:String};function z(e,i=t){return s(()=>e.size!==void 0?{fontSize:e.size in i?`${i[e.size]}px`:e.size}:null)}export{z as a,t as b,u};
